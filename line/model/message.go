7266e112b6c032254a5cfb7aba4c7a753806cd24
Parichaya <parichaya35@gmail.com>
2021-08-04 02:18:23 +0700

    format

--- line/model/message.go
+++ line/model/message.go
@@ -1,36 +1,35 @@
 package model
 
 type (
-
 	LineMessage struct {
-		Destination 	string 			`json:"destination"`
-		Events      	[]struct {
-			ReplyToken 		string 		`json:"replyToken"`
-			Type       		string 		`json:"type"`
-			Timestamp  		int64  		`json:"timestamp"`
-			Source    	 	struct {
-				Type   		string 		`json:"type"`
-				UserID 		string 		`json:"userId"`
-			} 							`json:"source"`
+		Destination string `json:"destination"`
+		Events      []struct {
+			ReplyToken string `json:"replyToken"`
+			Type       string `json:"type"`
+			Timestamp  int64  `json:"timestamp"`
+			Source     struct {
+				Type   string `json:"type"`
+				UserID string `json:"userId"`
+			} `json:"source"`
 			Message struct {
-				ID   		string 		`json:"id"`
-				Type 		string 		`json:"type"`
-				Text 		string 		`json:"text"`
-			} 							`json:"message"`
-		} 								`json:"events"`
+				ID   string `json:"id"`
+				Type string `json:"type"`
+				Text string `json:"text"`
+			} `json:"message"`
+		} `json:"events"`
 	}
 
 	ReplyMessage struct {
-		ReplyToken 			string 		`json:"replyToken"`
-		Messages   			[]Text 		`json:"messages"`
+		ReplyToken string `json:"replyToken"`
+		Messages   []Text `json:"messages"`
 	}
 
 	BroadcastMessage struct {
-		Messages   			[]Text 		`json:"messages"`
+		Messages []Text `json:"messages"`
 	}
 
 	Text struct {
-		Type 				string 		`json:"type"`
-		Text 				string 		`json:"text"`
+		Type string `json:"type"`
+		Text string `json:"text"`
 	}
 )
